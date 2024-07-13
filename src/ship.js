7a486bf1925d8f5f08cc3d74ee00e415d9e24ae0
lsbdevops <lsbdevops@gmail.com>
2024-07-13 17:42:48 +1000

    Add hit func to Ship class
    Hit function increments the number of hits dealt
    to a ship.

--- src/ship.js
+++ src/ship.js
@@ -3,4 +3,8 @@ export default class ship {
         this.length = length;
         this.hits = 0;
     }
+
+    hit() {
+        this.hits++;
+    }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,12 +1,23 @@
 import ship from './ship'
 
-test('Ship of length 1 created', () => {
+test('Ship of length 1 - created', () => {
     const testShip = new ship(1);
     expect(testShip).toEqual({ length: 1, hits: 0 });
 }) 
 
-test('Ship of length 5 created', () => {
+test('Ship of length 5 - created', () => {
     const testShip = new ship(5);
     expect(testShip).toEqual({ length: 5, hits: 0 });
 })
 
+test('Ship of length 1 - hit once', () => {
+    const testShip = new ship(1);
+    testShip.hit();
+    expect(testShip).toEqual({ length: 1, hits: 1 });
+}) 
+
+test('Ship of length 5 - hit once', () => {
+    const testShip = new ship(5);
+    testShip.hit();
+    expect(testShip).toEqual({ length: 5, hits: 1 });
+})
