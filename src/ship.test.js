6c69976450eea5ebd8932866acfa7f057c0a3e84
lsbdevops <lsbdevops@gmail.com>
2024-07-13 18:03:02 +1000

    Reduce duplication of ship obj creation in testing
    beforeEach hook used to create ships to reduce
    code duplication.

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,42 +1,43 @@
 import Ship from './ship'
 
+let testShip1;
+let testShip5;
+
+beforeEach(() => {
+    testShip1 = new Ship(1);
+    testShip5 = new Ship(5);
+})
+
 test('Ship of length 1 - created', () => {
-    const testShip = new Ship(1);
-    expect(testShip).toEqual({ length: 1, hits: 0 });
+    expect(testShip1).toEqual({ length: 1, hits: 0 });
 }) 
 
 test('Ship of length 5 - created', () => {
-    const testShip = new Ship(5);
-    expect(testShip).toEqual({ length: 5, hits: 0 });
+    expect(testShip5).toEqual({ length: 5, hits: 0 });
 })
 
 test('Ship of length 1 - hit once', () => {
-    const testShip = new Ship(1);
-    testShip.hit();
-    expect(testShip).toEqual({ length: 1, hits: 1 });
+    testShip1.hit();
+    expect(testShip1).toEqual({ length: 1, hits: 1 });
 }) 
 
 test('Ship of length 5 - hit once', () => {
-    const testShip = new Ship(5);
-    testShip.hit();
-    expect(testShip).toEqual({ length: 5, hits: 1 });
+    testShip5.hit();
+    expect(testShip5).toEqual({ length: 5, hits: 1 });
 })
 
 test('Ship of length 1 - not sunk', () => {
-    const testShip = new Ship(1);
-    expect(testShip.isSunk()).toBe(false);
+    expect(testShip1.isSunk()).toBe(false);
 }) 
 
 test('Ship of length 1 - sunk', () => {
-    const testShip = new Ship(1);
-    testShip.hit();
-    expect(testShip.isSunk()).toBe(true);
+    testShip1.hit();
+    expect(testShip1.isSunk()).toBe(true);
 }) 
 
 test('Ship of length 5 - sunk', () => {
-    const testShip = new Ship(5);
     for (let i = 0; i < 5; i++) {
-        testShip.hit();
+        testShip5.hit();
     }
-    expect(testShip.isSunk()).toBe(true);
+    expect(testShip5.isSunk()).toBe(true);
 }) 
