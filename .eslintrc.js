bc68d3adafffc317df5da094e991da1a5097d96f
lsbdevops <141811908+lsbdevops@users.noreply.github.com>
2024-07-13 17:07:54 +1000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      env: {
+        jest: true,
+      },
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx'
+      ],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
