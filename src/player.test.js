25c188bfe61e80f205cc7e9edf35e15aff14cdcd
lsbdevops <lsbdevops@gmail.com>
2024-09-08 22:57:46 +1000

    Add gameboard prop to player class
    A 10x10 gameboard is created for each player
    created.

--- src/player.test.js
+++ src/player.test.js
@@ -9,3 +9,8 @@ test('Create computer player', () => {
     const player = new Player('computer');
     expect(player).toEqual(expect.objectContaining({ type: 'computer' }));
 })
+
+test('Create human player with gameboard', () => {
+    const player = new Player('human');
+    expect(player).toEqual({type: 'human', gameboard: expect.objectContaining({ height: 10, width: 10 })});
+})
