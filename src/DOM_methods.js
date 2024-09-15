fbce1cdc0e20947b979a1bdce6cb7dec5155862a
lsbdevops <lsbdevops@gmail.com>
2024-09-15 23:09:20 +1000

    Hide opponent player's ships on gameboard
    The 'attacking' gameboard no longer shows the
    opponent's ships. The player can only see their
    own ships on the 'ships' gameboard.

--- src/DOM_methods.js
+++ src/DOM_methods.js
@@ -1,11 +1,11 @@
-function createCell(cellData) {
+function createCell(cellData, gameboardType) {
     const cell = document.createElement('div');
     cell.classList.add('game-cell');
 
     if (cellData.ship && cellData.isAttacked) {
         cell.classList.add('ship-attacked');
     }
-    else if (cellData.ship) {
+    else if (cellData.ship && gameboardType === 'ship') {
         cell.classList.add('ship');
     }
     else if (cellData.isAttacked) {
@@ -23,7 +23,7 @@ export default function renderGameboard(gameboard, type) {
     // Create required number of cells and append to wrapper element.
     for (let i = 0; i < gameboard.height; i += 1) {
         for (let j = 0; j < gameboard.width; j += 1) {
-            const cell = createCell(gameboard.boardCoordinates[i][j])
+            const cell = createCell(gameboard.boardCoordinates[i][j], type)
 
             if (type === 'attacking') {
                 cell.addEventListener('click', () => {
--- src/script.js
+++ src/script.js
@@ -4,5 +4,7 @@ import renderGameboard from './DOM_methods'
 
 const playerOne = new Player('human');
 const playerTwo = new Player('computer');
+playerOne.gameboard.placeShip(1, 1)
+playerTwo.gameboard.placeShip(3, 5, 3)
 renderGameboard(playerOne.gameboard, 'ship');
 renderGameboard(playerTwo.gameboard, 'attacking');
