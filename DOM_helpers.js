d8a058da7a50b860ddf943b23c38140f4e7c497a
lsbdevops <lsbdevops@gmail.com>
2024-10-02 08:20:29 +1000

    Move DOM_gameboard.js and DOM_helpers.js to src

--- DOM_helpers.js
+++
@@ -1,9 +0,0 @@
-function getRandomCoords() {
-    return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]
-}
-
-function getRandomOrientation() {
-    return ['horizontal', 'vertical'][Math.floor(Math.random() * 2)];
-}
-
-export {getRandomCoords, getRandomOrientation}
